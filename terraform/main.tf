b579a1243aeee4de110769ac6b9e2097db4bd918
Travis Hong <travis.j.hong@gmail.com>
2023-03-28 23:51:43 +1100

    add python scripts to ingress bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,3 +41,28 @@ resource "aws_s3_bucket" "gold" {
   bucket = "gwd-gold"
   force_destroy = true  # delete everything stored when destroyed
 }
+
+# Upload Python scripts to S3 bucket
+resource "aws_s3_object" "ingress_loader" {
+  bucket = aws_s3_bucket.scripts.id
+  key    = "ingress_loader.py"
+  source = "../code/ingress_loader.py"
+}
+
+resource "aws_s3_object" "ingress_to_bronze" {
+  bucket = aws_s3_bucket.scripts.id
+  key    = "ingress_to_bronze.py"
+  source = "../code/ingress_to_bronze.py"
+}
+
+resource "aws_s3_object" "bronze_to_silver" {
+  bucket = aws_s3_bucket.scripts.id
+  key    = "bronze_to_silver.py"
+  source = "../code/bronze_to_silver.py"
+}
+
+resource "aws_s3_object" "silver_to_gold" {
+  bucket = aws_s3_bucket.scripts.id
+  key    = "silver_to_gold.py"
+  source = "../code/silver_to_gold.py"
+}
